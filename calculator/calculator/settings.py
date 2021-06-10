1d100660e963c8854b7d35c7df7ccb4a246b4d3a
professorbruh <62765220+professorbruh@users.noreply.github.com>
2021-06-10 14:02:56 +0530

    Production email correction

--- calculator/calculator/settings.py
+++ calculator/calculator/settings.py
@@ -129,4 +129,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'gmccalc@gmail.com'
-EMAIL_HOST_PASSWORD = 'gmccalc123'
+EMAIL_HOST_PASSWORD = 'yvqwrkafbrfzqfiz'
